bb51805ab93902818c909a6f807759afebde2b3f
Jacob Smith <jsmithdenverdev@gmail.com>
2023-01-03 02:14:52 -0700

    Removed commen

--- host/src/components/navbar.tsx
+++ host/src/components/navbar.tsx
@@ -6,14 +6,6 @@ const NavItem = ({ to, children }: any) => (
       margin: "0px 10px",
     }}
   >
-    {/* TODO: This is super obnoxious, but routes shared under a common remote
-     * aren't updating correctly using just client-side routing. The entire
-     * document needs to be refreshed to ensure the appropriate remote route
-     * is rendered (e.g., client side routing doesn't update between / and /about)
-     * but forcing a browser refresh ensures the appropriate route is rendered.
-     * This also means that the entire document is repainted on navigating,
-     * so an obnoxious flash occurs on the screen.
-     */}
     <Link to={to}>{children}</Link>
   </li>
 );
